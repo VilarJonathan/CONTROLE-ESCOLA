6f0cff5ec54cea701c0b67524002e789c53317d7
Jeanderson <Jeanderson@DESKTOP-10QPC6Q>
2017-05-11 20:26:44 -0300

    usando a classe no log, no try e catch

--- src/controller/HomeController.java
+++ src/controller/HomeController.java
@@ -7,6 +7,7 @@ import java.util.logging.Logger;
 import javafx.fxml.FXML;
 import jeanderson.controller.componentes.Inicializador;
 import jeanderson.controller.control.ControlStage;
+import util.Log;
 
 /**
  * Classe de controle da Home
@@ -21,22 +22,27 @@ public class HomeController extends Inicializador {
     public void initialize(URL location, ResourceBundle resources) {
         try {
             this.telaCadastro = ControlStage.newBuilder()
-                    .addClassController(new CadastroContatoController())
                     .addNameFromFXML("CadastroContato").build();
 
         } catch (Exception ex) {
-            Logger.getLogger(HomeController.class.getName()).log(Level.SEVERE, null, ex);
+            //log será salvo em um arquivo txt dentro do projeto
+            Log.salvaLogger(this.getClass().getName(),"initialize()", ex);
         }
 
     }
 
+    /**
+     * Método é chamado no clique do menuitem Contatos.
+     * Faz a exibição da Tela de cadastro.
+     */
     @FXML
     public void chamarTelaCadastroContato() {
         try {
             telaCadastro.show();
 
         } catch (Exception ex) {
-            Logger.getLogger(HomeController.class.getName()).log(Level.SEVERE, null, ex);
+            //log será salvo em um arquivo txt dentro do projeto
+            Log.salvaLogger(this.getClass().getName(),"chamarTelaCadastroContato()", ex);
         }
 
     }
