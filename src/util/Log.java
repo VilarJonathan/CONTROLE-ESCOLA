32f86792b42e468881e4c545ca1a86c71fa7cd21
Jeanderson <Jeanderson@DESKTOP-10QPC6Q>
2017-05-11 23:10:46 -0300

    Feito correção, e implementação do uso de Dialog.

--- src/util/Log.java
+++ src/util/Log.java
@@ -16,6 +16,8 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import jeanderson.controller.util.DialogFX;
+import jeanderson.controller.util.DialogType;
 
 /**
  * Classe responsável por salvar log em forma de arquivo de texto.
@@ -25,25 +27,29 @@ import java.util.logging.Logger;
 public class Log {
 
     /**
-     *  Método pega a exceção lançada, e grava em um arquivo de Texto.
+     * Método pega a exceção lançada, e grava em um arquivo de Texto. e também
+     * exibi um Dialog informando o erro.
+     *
      * @param className Nome da Classe de Exceção.
-     * @param ex  Exceção lançada.
+     * @param methodName Nome do método ou código onde ocorreu a exception.
+     * @param ex Exceção lançada.
      */
-    public static void salvaLogger(String className, String methodName, Exception ex) {
+    public static void salvaLogger(String className, String whereException, Exception ex) {
+        DialogFX.showMessage("Houve um erro no programa, por favor consulte o log.txt", "Uma exceção lançada", DialogType.ERRO);
         try {
             String dataDoOcorrido = LocalDateTime.now().format(DateTimeFormatter.ofPattern("dd/MM/yyyy 'ás' HH:mm"));
             List<String> msg = new ArrayList<>();
             msg.add("Ocorreu uma exceção na data: " + dataDoOcorrido);
             msg.add("Na Classe: " + className);
             msg.add("Motivo: " + ex.getMessage());
-            msg.add("No método: " + methodName);
+            msg.add("Onde: " + whereException);
             msg.add("");
 
             File arquivo = new File("log.txt");
             if (!arquivo.exists()) {
                 arquivo.createNewFile();
             }
-            
+
             Files.write(Paths.get(arquivo.getPath()), msg, StandardOpenOption.APPEND);
         } catch (IOException ex1) {
             Logger.getLogger(Log.class.getName()).log(Level.SEVERE, null, ex1);
