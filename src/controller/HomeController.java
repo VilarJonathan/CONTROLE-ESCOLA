2890e71dea4e15f470f5747149a852e46c52240d
Jeanderson <Jeanderson@DESKTOP-10QPC6Q>
2017-05-16 17:25:07 -0300

    Adicionado mais dois evento de action.

--- src/controller/HomeController.java
+++ src/controller/HomeController.java
@@ -19,10 +19,11 @@ public class HomeController extends Inicializador {
 
     private ControlStage<CadastroContatoController> telaCadastro;
     private ControlStage<CadastroContatoController> telaCadastroCurso;
+    private ControlStage<ConfiguracoesController> telaConfiguracao;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        
+
         try {
             this.telaCadastro = ControlStage.newBuilder()
                     .addClassController(new CadastroContatoController())
@@ -31,17 +32,22 @@ public class HomeController extends Inicializador {
                     .addClassController(new CadastroContatoController())
                     .addNameFromFXML("CadastroCurso")
                     .addTitleStage("Cadastro de Cursos").build();
+            this.telaConfiguracao = new ControlStageBuilder<>()
+                    .addClassController(new ConfiguracoesController())
+                    .addNameFromFXML("Configuracoes")
+                    .addTitleStage("Configuração do Banco de Dados")
+                    .build();
 
         } catch (Exception ex) {
             //log será salvo em um arquivo txt dentro do projeto
-            Log.salvaLogger(this.getClass().getName(),"initialize()", ex);
+            Log.salvaLogger(this.getClass().getName(), "initialize()", ex);
         }
 
     }
 
     /**
-     * Método é chamado no clique do menuitem Contatos.
-     * Faz a exibição da Tela de cadastro.
+     * Método é chamado no clique do menuitem Contatos. Faz a exibição da Tela
+     * de cadastro.
      */
     @FXML
     public void chamarTelaCadastroContato() {
@@ -51,19 +57,32 @@ public class HomeController extends Inicializador {
             this.telaCadastro.getController().carregarListaDeCursos();
         } catch (Exception ex) {
             //log será salvo em um arquivo txt dentro do projeto
-            Log.salvaLogger(this.getClass().getName(),"chamarTelaCadastroContato()", ex);
+            Log.salvaLogger(this.getClass().getName(), "chamarTelaCadastroContato()", ex);
         }
 
     }
+
     /**
      * Método chama a Tela de Cadastro de Cursos.
      */
     @FXML
-    public void chamarTelaCadastroCurso(){
+    public void chamarTelaCadastroCurso() {
         try {
             this.telaCadastroCurso.show(HomeController.class);
         } catch (Exception ex) {
-            Log.salvaLogger(this.getClass().getName(),"chamarTelaCadastroCurso()", ex);
+            Log.salvaLogger(this.getClass().getName(), "chamarTelaCadastroCurso()", ex);
+        }
+    }
+    
+    /**
+     * Método chama a tela de configurações.
+     */
+    @FXML
+    public void chamarTelaConfiguracoes(){
+        try {
+            this.telaConfiguracao.show(HomeController.class);
+        } catch (Exception ex) {
+            Log.salvaLogger(this.getClass().getName(), "chamarTelaConfiguracoes()", ex);
         }
     }
 
