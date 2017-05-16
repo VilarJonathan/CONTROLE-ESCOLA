b5166ad72ba4675a9a08c4c06706a2c0c4429e1f
Jeanderson <Jeanderson@DESKTOP-10QPC6Q>
2017-05-16 17:31:10 -0300

    Classes e interface responsável por conversão de dados. foram movidos.

---
+++ src/util/converters/LocalDateAttributeConverter.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package util.converters;
+
+import java.sql.Date;
+import java.time.LocalDate;
+import javax.persistence.AttributeConverter;
+import javax.persistence.Converter;
+
+/**
+ * Classe que faz a conversão automatica de LocalDate para Date e virse e versa.
+ * Necessario para versão anteriores ao Hibernate 5.
+ *
+ * @author Jeanderson
+ */
+@Converter(autoApply = true)
+public class LocalDateAttributeConverter implements AttributeConverter<LocalDate, Date> {
+
+    @Override
+    public Date convertToDatabaseColumn(LocalDate localDate) {
+        return (localDate == null ? null : Date.valueOf(localDate));
+    }
+
+    @Override
+    public LocalDate convertToEntityAttribute(Date date) {
+        return (date == null ? null : date.toLocalDate());
+    }
+
+}
