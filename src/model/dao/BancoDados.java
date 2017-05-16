dd605b92b4599dd2f5f61bf8732080334474c7cc
Jeanderson <Jeanderson@DESKTOP-10QPC6Q>
2017-05-15 22:58:59 -0300

    Implementado a classe responsável pelas transações do banco de dados.

--- src/model/dao/BancoDados.java
+++ src/model/dao/BancoDados.java
@@ -5,25 +5,37 @@
  */
 package model.dao;
 
+import org.hibernate.HibernateException;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
 import util.HibernateUtil;
+import util.Log;
 
 /**
  *
  * @author Jeanderson
  */
 public class BancoDados {
+
     /**
      * Método que utilizando o Hibernate faz a persistencia do dado no banco.
+     *
      * @param objeto Objeto a ser persistido.
+     * @return Informa se a operação ocorreu com sucesso ou não.
      */
-    public static void salvar(Object objeto){
+    public static boolean salvar(Object objeto) {
         Session sessao = HibernateUtil.getSessionFactory().openSession();
-        Transaction transacao = sessao.beginTransaction();
-        sessao.save(objeto);
-        transacao.commit();
-        sessao.close();
-        HibernateUtil.getSessionFactory().close();
+        try {
+            Transaction transacao = sessao.beginTransaction();
+            sessao.save(objeto);
+            transacao.commit();
+            return true;
+        } catch (HibernateException ex) {
+            Log.salvaLogger(BancoDados.class.getName(), "salvar()", ex);
+            return false;
+        } finally {
+            sessao.close();
+            HibernateUtil.getSessionFactory().close();
+        }
     }
 }
