f3331ea27a17d61c2d33a8b69108ef57d6c25c6f
Jeanderson <Jeanderson@DESKTOP-10QPC6Q>
2017-05-16 17:28:55 -0300

    Movido para outro pacote.

---
+++ src/util/StringConverter.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package util;
+
+/**
+ * Interface que vai obrigar as todos enum a implementar o método que retorna uma StringConverter.
+ * @author Jeanderson
+ */
+public interface StringConverter {
+    /**
+     * Deve retorna uma StringConverter implementada de acordo com a Classe.
+     * @return StringConverter.
+     */
+    public javafx.util.StringConverter getConverter();
+}
