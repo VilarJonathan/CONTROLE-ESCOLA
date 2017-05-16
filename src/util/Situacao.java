955a7cc36535f368677bb1e447ef6709af88f928
Jeanderson <Jeanderson@DESKTOP-10QPC6Q>
2017-05-16 12:07:55 -0300

    implementado enum que representa as situações do aluno, ou contato.

---
+++ src/util/Situacao.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package util;
+
+/**
+ *Enum que representa as situações gerais como de contato.
+ * @author Jeanderson
+ */
+public enum Situacao {
+    TEM_INTERESSE, SEM_INTERESSE, FAZER_VISITA;
+
+    @Override
+    public String toString() {
+        switch (this) {
+            case TEM_INTERESSE:
+                return "Tem Interesse";
+            case SEM_INTERESSE:
+                return "Não tem interesse";
+            case FAZER_VISITA:
+                return "Vai Fazer uma visita";
+            default:
+                return "";
+        }
+
+    }
+
+}
