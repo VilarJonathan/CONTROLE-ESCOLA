5d5833d5194ce73dfefb489c5e4354a871e3dc91
Jeanderson <Jeanderson@DESKTOP-10QPC6Q>
2017-05-18 00:12:36 -0300

    Feito implementação final.

--- src/controller/ferramentas/ConfiguracoesController.java
+++ src/controller/ferramentas/ConfiguracoesController.java
@@ -5,13 +5,21 @@
  */
 package controller.ferramentas;
 
+import controller.HomeController;
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javafx.fxml.FXML;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import jeanderson.controller.componentes.Inicializador;
+import jeanderson.controller.control.ControlStage;
+import jeanderson.controller.util.DialogFX;
 import model.ConfigHibernateXML;
+import org.hibernate.HibernateException;
+import util.HibernateUtil;
+import util.Log;
 import util.xml.HibernateConfig;
 
 /**
@@ -32,26 +40,49 @@ public class ConfiguracoesController extends Inicializador {
     private TextField txtEndereco;
     private HibernateConfig hibernateConfig;
     private ConfigHibernateXML xml;
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        
+
     }
+
     @FXML
-    public void actionSalvar(){
+    public void actionSalvar() {
         this.hibernateConfig.getSessionFactory().setUrlConfig(this.txtNomeDoBanco.getText().trim(), txtEndereco.getText().trim(), txtPorta.getText().trim());
         this.hibernateConfig.getSessionFactory().setUsername(this.txtUsuario.getText().trim());
         this.hibernateConfig.getSessionFactory().setPassword(this.txtSenha.getText().trim());
         this.xml.salvarConfig(this.hibernateConfig);
+        if (DialogFX.showConfirmation("Configurações Salvas! É necessário reiniciar para que tenha efeito!\n Deseja fechar agora?", "Necessário Reinicialização.")) {
+            
+            try {
+                ControlStage<HomeController> controlHome = ControlStage.getAllSeeControl(HomeController.class);
+                controlHome.getStage().close();
+                HibernateUtil.getSessionFactory().close();
+            } catch (Exception ex) {
+                Log.salvaLogger(getClass().getName(), "actionCancelar()", ex);
+            }catch(NoClassDefFoundError e){
+                Logger.getLogger(Log.class.getName()).log(Level.SEVERE, null, e);
+                System.exit(0);
+            }
+        }
     }
+
     @FXML
-    public void actionCancelar(){
-        
+    public void actionCancelar() {
+        ControlStage<HomeController> controlHome;
+        try {
+            controlHome = ControlStage.getAllSeeControl(HomeController.class);
+            controlHome.getController().telaConfiguracao.getStage().close();
+        } catch (Exception ex) {
+            Log.salvaLogger(getClass().getName(), "actionCancelar()", ex);
+        }
+
     }
-    
+
     /**
      * Carrega as configuracoes do arquivo xml do hibernate.
      */
-    public void carregarConfiguracoes(){
+    public void carregarConfiguracoes() {
         this.xml = new ConfigHibernateXML();
         this.hibernateConfig = xml.getConfigXMl();
     }
