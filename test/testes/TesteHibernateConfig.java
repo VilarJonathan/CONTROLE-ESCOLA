f737ae132671f18c612e85bb61c7389467fa8069
Jeanderson <Jeanderson@DESKTOP-10QPC6Q>
2017-05-18 22:19:03 -0300

    pasta de testes ignorar.

---
+++ test/testes/TesteHibernateConfig.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package testes;
+
+import util.HibernateUtil;
+
+/**
+ *
+ * @author Jeanderson
+ */
+public class TesteHibernateConfig {
+    public static void main(String[] args) {
+        
+    }
+    
+}
